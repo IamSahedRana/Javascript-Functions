14c9a06b49c4113dd4d1ae8f567a55c099d2489e
IamSahedRana <sahed.vmc@gmail.com>
2026-07-25 13:38:21 +0600

    Use Object as parameters in Function

--- different.js
+++ different.js
@@ -25,3 +25,25 @@ function makeItDoubled(num){
 const enterNum = [30,40,50,60,70,80]
 const double = makeItDoubled(enterNum);
 console.log('the first doubled is :', double);
+
+// use object in function 
+
+const student = {
+     name : 'Sahed',
+     subject : 'Physics',
+     Marks : 95
+}
+
+function isAPlus(student){
+     console.log('Get the Object', student);
+     const hisMarks = student.Marks;
+     if (hisMarks > 80){
+          console.log('He got A Plus');
+     }
+     else {
+          console.log('He got this: ', hisMarks);
+     }
+
+}
+
+const num = isAPlus(student);
